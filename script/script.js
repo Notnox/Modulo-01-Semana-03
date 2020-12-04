c309d825d000ee4c0f81ab50ed151df4001319ec
Notnox <74467549+Notnox@users.noreply.github.com>
2020-12-04 18:18:25 -0300

    Adicionado JS
    - Retirado opção para salvar listas

---
+++ script/script.js
@@ -0,0 +1,71 @@
+function adicionar(){
+    let texto = window.document.querySelector('input#textoentrada')
+
+    if (texto.value == ''){return window.alert('[ERRO] É necessário informar uma tarefa!')}
+
+    let tarefaTexto = document.createTextNode(texto.value)
+
+    if (localStorage.totalTarefas == undefined) {
+        localStorage.setItem('totalTarefas',1)
+    } else {
+        let vLS = Number(localStorage.totalTarefas)
+        vLS++
+        localStorage.setItem('totalTarefas',vLS)
+    }
+
+    let vLinha = 'div' + localStorage.totalTarefas
+    let vLabel = 'lb' + localStorage.totalTarefas
+
+    let tarefaBotao = document.createTextNode('X')
+
+    let linha = document.createElement('div')
+    linha.setAttribute('class', 'tarefa')
+    linha.setAttribute('id', vLinha)
+
+    let check = document.createElement('input')
+    check.setAttribute('type', 'checkbox')
+    check.setAttribute('id', 'ch'+ vLabel)
+    check.onclick = function (){realizado(vLabel)}
+
+    let tarefa = document.createElement('p')
+    tarefa.setAttribute('id', vLabel)
+    tarefa.appendChild(tarefaTexto)
+
+    let botao = document.createElement('button')
+    botao.setAttribute('title', 'Excluir tarefa')
+    botao.onclick = function (){excluir(vLinha,vLabel)}
+    botao.appendChild(tarefaBotao)
+
+    let tarefas = window.document.querySelector('section#tarefas')
+  
+    tarefas.appendChild(linha)
+
+    linha.appendChild(check)
+    linha.appendChild(tarefa)
+    linha.appendChild(botao)
+
+    texto.value = ''
+}
+
+function realizado(tarefa){
+    let feito = window.document.querySelector('input#' + 'ch' + tarefa)
+    let vtarefa = window.document.querySelector('p#' + tarefa)
+    
+    if (feito.checked == true){
+        vtarefa.style = "text-decoration: line-through 4px"
+    } else {
+        vtarefa.style = "text-decoration: none"
+    }
+}
+
+function excluir(linha,vLinha){
+    let vTarefa = window.document.querySelector('p#' + vLinha)
+
+    let escolha = window.confirm(`Você tem certeza que deseja remover a tarefa ${vTarefa.innerHTML}?`)
+
+    if (escolha) {
+        let tarefa = window.document.querySelector('div#' + linha)
+
+        tarefa.parentNode.removeChild(tarefa)
+    }
+}
